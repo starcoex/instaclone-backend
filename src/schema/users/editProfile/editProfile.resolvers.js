abb32c4f28fad8d7ca4471a02f12eab6920ff6d3
starcoex <69073390+starcoex@users.noreply.github.com>
2023-04-21 21:06:17 +0900

    user model token part1

--- src/schema/users/editProfile/editProfile.resolvers.js
+++ src/schema/users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,43 @@
+import client from '../../../prismaClient/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 export default {
   Mutation: {
-    editProfile: () => {
-      console.log("hi")
-    }
+    editProfile: async (
+      _,
+      { firstName, lastName, userName, email, password, token }
+    ) => {
+      console.log(firstName, lastName, userName, email, password, token);
+      const { id } = jwt.verify(token, process.env.PRIVATE_KEY);
+      console.log('token', id);
+      let passwordHash = null;
+      if (password) {
+        passwordHash = await bcrypt.hash(password, 5);
+      }
+      // const passwordHash = await bcrypt.hash(password, 5);
+
+      const updateUser = await client.user.update({
+        where: { id: id },
+        data: {
+          firstName,
+          lastName,
+          userName,
+          email,
+          // password: passwordHash,
+          ...(passwordHash && { password: passwordHash }),
+        },
+      });
+      if (updateUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: 'not update',
+        };
+      }
+    },
   },
 };
